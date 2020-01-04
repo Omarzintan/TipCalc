8f6fee1dd1f39c0c30eed9842f4cf12e53dd0af9
Omarzintan <zintanomar@gmail.com>
2020-01-04 14:00:17 -0800

    Initial Commit

---
+++ TipCalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipCalc
+//
+//  Created by Omar Zintan Mwinila-Yuori on 04/01/2020.
+//  Copyright © 2020 Omar Zintan Mwinila-Yuori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
